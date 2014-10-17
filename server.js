8bed41f042c9779b4dbe5a557a7421fc6c18ed89
Hiren Kavad <Hirenkavad@ymail.com>
2014-10-18 00:15:10 +0530

    Fix
    I don't know what i did but it's now working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var file = new(static.Server)();
 var app = http.createServer(function(req, res) {
     file.serve(req, res);
-}).listen(3000);
+}).listen(process.env.PORT || 3000);
 
 var io = require('socket.io').listen(app);
 io.sockets.on('connection', function(socket) {
